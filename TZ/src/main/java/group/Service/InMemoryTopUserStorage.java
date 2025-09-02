44d2a618b32e76f87197baac633c4827e2b75e0f
Aqura <aqurawe@gmail.com>
2025-09-02 10:09:51 +0200

    small update

--- TZ/src/main/java/group/Service/InMemoryTopUserStorage.java
+++ TZ/src/main/java/group/Service/InMemoryTopUserStorage.java
@@ -5,13 +5,11 @@ import group.Service.TopUserStorage.TopUserStorage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 
-import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 @Service
 public class InMemoryTopUserStorage implements TopUserStorage {
@@ -26,33 +24,18 @@ public class InMemoryTopUserStorage implements TopUserStorage {
 			.reversed()
 			.thenComparing(User::getId);
 
-	private final ConcurrentHashMap<Integer, TreeSet<User>> userInfoStorage = new ConcurrentHashMap<>();
-	private final ConcurrentHashMap<Integer, TreeSet<User>> levelInfoStorage = new ConcurrentHashMap<>();
+	private final ConcurrentHashMap<Integer, ConcurrentSkipListSet<User>> userInfoStorage = new ConcurrentHashMap<>();
+	private final ConcurrentHashMap<Integer, ConcurrentSkipListSet<User>> levelInfoStorage = new ConcurrentHashMap<>();
 
 	@Override
 	public void cache(User user) {
-		userInfoStorage.compute(user.getId(), (key, set) -> {
-			if (set == null) {
-				set = new TreeSet<>(userInfoComparator);
-			}
-			set.add(user);
-			trimTop(set);
-			return set;
-		});
-
-		levelInfoStorage.compute(user.getLevelId(), (key, set) -> {
-			if (set == null) {
-				set = new TreeSet<>(levelInfoComparator);
-			}
-			set.add(user);
-			trimTop(set);
-			return set;
-		});
+		cacheToMap(userInfoStorage, user.getId(), user, userInfoComparator);
+		cacheToMap(levelInfoStorage, user.getLevelId(), user, levelInfoComparator);
 	}
 
 	@Override
 	public List<User> getUserInfo(Integer userId) {
-		TreeSet<User> set = userInfoStorage.get(userId);
+		Set<User> set = userInfoStorage.get(userId);
 		if(set == null) {
 			return List.of();
 		}
@@ -61,17 +44,30 @@ public class InMemoryTopUserStorage implements TopUserStorage {
 
 	@Override
 	public List<User> getLevelInfo(Integer levelId) {
-		TreeSet<User> set = levelInfoStorage.get(levelId);
+		Set<User> set = levelInfoStorage.get(levelId);
 		if(set == null) {
 			return List.of();
 		}
 		return new ArrayList<>(set);
 	}
 
-	private void trimTop(TreeSet<User> set) {
-		while (set.size() > topOffside) {
-			set.pollLast();
-		}
+	private void cacheToMap(
+			ConcurrentHashMap<Integer, ConcurrentSkipListSet<User>> map,
+			Integer key,
+			User user,
+			Comparator<User> comparator) {
+
+		map.compute(key, (k, set) -> {
+			if (set == null) {
+				set = new ConcurrentSkipListSet<>(comparator);
+			}
+			set.add(user);
+			if (set.size() > topOffside) {
+				set.pollLast();
+			}
+
+			return set;
+		});
 	}
 }
 
